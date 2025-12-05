20b3e191ba7a27a55dd09ea74fe37f96e1eebc52
MaikHvz <maic.hernandez.dev@gmail.com>
2025-12-05 13:17:04 -0300

    Update SecurityConfig.java

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
             "http://localhost:5173",
             "http://localhost:5174",
             "http://localhost:5175",
-            "https://levelup-auth-production.up.railway.app"
+            "https://levelup-auth-production.up.railway.app",
+            "http://levelup-auth-production.up.railway.app/api/auth/register"
         ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
